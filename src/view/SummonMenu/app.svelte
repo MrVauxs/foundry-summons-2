8eec9aed9315a95b25d92922b3bffadacb5e1485
mrvauxs <mrvauxs@gmail.com>
2025-07-08 13:09:06 +0200

    this sucks

--- src/view/SummonMenu/app.svelte
+++ src/view/SummonMenu/app.svelte
@@ -6,7 +6,7 @@
 
 <article class="root">
 	<aside class="sidebar"> aside </aside>
-	<article class="main"> a </article>
+	<article class="main"> main content </article>
 </article>
 
 <style lang="postcss">
@@ -14,13 +14,30 @@
 		display: flex;
 		flex-direction: row;
 		gap: 0.5rem;
+		min-height: 100%;
+		width: 100%;
 	}
 
 	.sidebar {
+		flex: 0 0 25%;
+		border: 2px solid transparent;
+		border-radius: 0.5rem;
+		background:
+			radial-gradient(circle at 50% 250%, #0000007f, #ffffff7f) padding-box,
+			linear-gradient(#000000, #ffffff) border-box;
 
+		padding: 0.25rem;
 	}
 
 	.main {
+		flex: 1;
+		border: 2px solid transparent;
+		border-radius: 0.5rem;
+		background:
+			radial-gradient(circle at 50% 250%, #0000007f, #ffffff7f) padding-box,
+			linear-gradient(#000000, #ffffff) border-box;
+
+		padding: 0.25rem;
 
 	}
 </style>
